bea8202ad0d2b22dec01886017d625974c5e99c9
conradprogrammer <conradprogrammer@gmail.com>
2025-03-22 06:45:17 -0700

    Update frmStrategy30.Designer.cs

--- Forms/frmStrategy30.Designer.cs
+++ Forms/frmStrategy30.Designer.cs
@@ -35,58 +35,7 @@
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Dock = Wisej.Web.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(953, 100);
-            this.panel1.TabIndex = 0;
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = Wisej.Web.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 100);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
-            this.splitContainer1.Size = new System.Drawing.Size(953, 453);
-            this.splitContainer1.SplitterDistance = 317;
-            this.splitContainer1.TabIndex = 1;
-            // 
-            // treeView1
-            // 
-            this.treeView1.CheckBoxes = true;
-            this.treeView1.Dock = Wisej.Web.DockStyle.Fill;
-            this.treeView1.Location = new System.Drawing.Point(0, 0);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(317, 453);
-            this.treeView1.TabIndex = 0;
-            // 
-            // frmStrategy30
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
-            this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(953, 553);
-            this.Controls.Add(this.splitContainer1);
-            this.Controls.Add(this.panel1);
-            this.Name = "frmStrategy30";
-            this.Text = "frmStrategy30";
-            this.Load += new System.EventHandler(this.frmStrategy30_Load);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
-            this.ResumeLayout(false);
-
-        }
-
-        #endregion
-
-        private Wisej.Web.Panel panel1;
-        private Wisej.Web.SplitContainer splitContainer1;
+            
         private Wisej.Web.TreeView treeView1;
     }
 }
